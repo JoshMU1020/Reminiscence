9edeee654c353cf1c1ce18e72a682b826a7547b5
JoshMU1020 <joshmu2000@gmail.com>
2023-11-24 21:54:51 -0500

    Add functionalities for drag and dropping images and creating notes in 2 by 2 pages. Added scroll wheel view of created pages.

--- MyScrapBookApp.py
+++ MyScrapBookApp.py
@@ -1,6 +1,64 @@
+import os
+import re
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.factory import Factory
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+from kivy.uix.image import Image
+from kivy.uix.videoplayer import VideoPlayer
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+
+
+class DropZone(FloatLayout):
+    def __init__(self, **kwargs):
+        super(DropZone, self).__init__(**kwargs)
+        self.drop_area = None
+        self.add_text_input()
+
+    def update_drop_zone(self, *args):
+        self.drop_area = self.pos + self.size
+
+    def _on_file_drop(self, window, file_path, x, y):
+        print(f"File dropped: {file_path}")
+        print(f"Drop position: {x}, {y}")
+        # Check if the drop is within this widget's bounds
+        self.handle_dropped_file(file_path)
+
+    def handle_dropped_file(self, file_path):
+        decoded_file_path = file_path.decode('utf-8')
+        if decoded_file_path.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+            self.display_image(decoded_file_path)
+
+    def display_image(self, file_path):
+        print(f"Displaying image in DropZone at {self.pos} with size {self.size}")
+        self.clear_widgets()
+        image = Image(source=file_path, keep_ratio=True, allow_stretch=True,
+                      size_hint=(1, 1), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        self.add_widget(image)
+
+    def clear_contents(self):
+        self.clear_widgets()
+
+    def add_text_input(self):
+        self.clear_widgets()
+        text_input = TextInput(
+            hint_text='Enter your note here or Drag and Drop image',
+            size_hint=(1, 1),
+            halign='center',
+            padding_y=(self.height * 0.4, 0),
+            background_color=(243/255, 170/255, 107/255, 1),
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            multiline=True
+        )
+        text_input.bind(on_text=self.on_text)
+        self.add_widget(text_input)
+
+    def on_text(self, instance, value):
+        instance.cursor = (0, 0)  # Reset cursor to start
 
 
 class WelcomeScreen(Screen):
@@ -42,10 +100,21 @@ class ViewPages(Screen):
         - Return to MainMenu screen.
     - A scrolling view of all created pages associated with the current user.
     '''
-    pass  # Page display layout and logic
+    def load_images(self):
+        directory = 'saved_collages'
+        if not os.path.exists(directory):
+            return  # Directory does not exist
+
+        container = self.ids.pages_container
+        container.clear_widgets()  # Clear existing images if any
+        for filename in sorted(os.listdir(directory)):
+            if filename.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                file_path = os.path.join(directory, filename)
+                img = Image(source=file_path, size_hint_y=None, height=400)  # Set a fixed height for each image
+                container.add_widget(img)
 
 
-class TemplateSelect(Screen):
+class TemplateSelect(Screen):  # MAY DROP IN FUTURE---------------------------------------------------------------------
     '''
     This screen will contain:
     - Return button:
@@ -78,10 +147,20 @@ class PageCreation(Screen):
         - user confirms that the page is complete.
         - Page is then placed into a page directory associated with the current user to be viewed from ViewPages screen.
     '''
+    def handle_drop(self, drop_zone, file_path):
+        drop_zone.handle_dropped_file(file_path)
+
     pass  # page creation menu layout and logic
 
+''' 
+def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    self.drop_area = FileDropWidget(size_hint=(0.5, 0.5), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+    self.add_widget(self.drop_area)
+    '''
 
-class DataScrolling(Screen):
+
+class DataScrolling(Screen):  # MAY DROP IN FUTURE----------------------------------------------------------------------
     '''
     This screen will contain:
     - Return button:
@@ -98,6 +177,9 @@ class DataScrolling(Screen):
 
 
 class MyScrapbookApp(App):
+    # A list to store the saved pages
+    saved_pages = []
+
     def build(self):
         sm = ScreenManager()
         sm.add_widget(WelcomeScreen(name='welcome'))
@@ -106,6 +188,8 @@ class MyScrapbookApp(App):
         sm.add_widget(TemplateSelect(name='templateSelect'))
         sm.add_widget(PageCreation(name='pageCreation'))
         sm.add_widget(DataScrolling(name='dataScrolling'))
+        # ... setup screens and drop zones ...
+        Window.bind(on_drop_file=self.on_file_drop)
         return sm
 
     def open_create_account_popup(self):
@@ -132,10 +216,54 @@ class MyScrapbookApp(App):
         print(note)
         self.create_note_popup.dismiss()
 
-    def switch_to_data_scrolling(self, data_type):
-        # Here, you can add logic to prepare the DataScrolling screen based on the data_type ('image' or 'video')
-        # For instance, setting a property on that screen to indicate what data to display
+    def switch_to_data_scrolling(self, data_type):  # MAY DROP IN FUTURE------------------------------------------------
+        # Here, add logic to prepare the DataScrolling screen based on the data_type ('image' or 'video')
         self.root.current = 'dataScrolling'
+
+    def on_file_drop(self, window, file_path, x, y):
+        print(f"File dropped at global coordinates: {x}, {y}")
+        y = Window.height - y
+        for drop_zone in self.get_drop_zones(self.root):
+            # Use the original global coordinates
+            if drop_zone.collide_point(x, y):
+                print(f"Drop zone hit at: {drop_zone.pos}")
+                drop_zone._on_file_drop(window, file_path, x, y)
+                return
+
+    def get_drop_zones(self, widget):
+        drop_zones = []
+        if isinstance(widget, DropZone):
+            drop_zones.append(widget)
+            print(f"DropZone found at {widget.pos} with size {widget.size}")
+        if hasattr(widget, 'children'):
+            for child in widget.children:
+                drop_zones.extend(self.get_drop_zones(child))
+        return drop_zones
+
+    def get_next_collage_number(self, directory):
+        highest_num = 0
+        for filename in os.listdir(directory):
+            match = re.match(r"collage(\d+)\.png", filename)
+            if match:
+                num = int(match.group(1))
+                highest_num = max(highest_num, num)
+        return highest_num + 1
+
+    def save_collage(self, layout):
+        directory = 'saved_collages'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        next_number = self.get_next_collage_number(directory)
+        file_path = os.path.join(directory, f'collage{next_number}.png')
+
+        layout.export_to_png(file_path)
+        self.clear_all_drop_zones()
+        print(f"Collage saved as: {file_path}")
+
+    def clear_all_drop_zones(self):
+        for drop_zone in self.get_drop_zones(self.root):
+            drop_zone.clear_contents()
 
 
 if __name__ == "__main__":
